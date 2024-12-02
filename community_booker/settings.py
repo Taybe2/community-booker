8ac11ea062ac34ea2ecac123c5da614687a9032c
Taybe Muharem <taybemuharem@gmail.com>
2024-12-02 12:52:33 +0000

    Add Secret KEY

--- community_booker/settings.py
+++ community_booker/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--gm48gg$l488y&bn8%dp0(27x@0jp+p8a-y6i(xz27w)qlff_#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
                     '8000-taybe2-communitybooker-s4z5jpwrufu.ws.codeinstitute-ide.net',
